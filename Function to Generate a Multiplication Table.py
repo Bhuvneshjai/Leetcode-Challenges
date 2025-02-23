e011d295c4040433122bffb9dbddaf9e2cffd01a
Bhuvneshjai <bhuvneshjain2000@gmail.com>
2025-02-23 19:44:32 +0530

    Function used lambda

--- Function to Generate a Multiplication Table.py
+++ Function to Generate a Multiplication Table.py
@@ -6,9 +6,16 @@ class Solution:
     def table(self, num: int) -> list:
         return [num * i for i in range(1,11)]
 
+    def table_lambda(self, num: int) -> list:
+        table = lambda num: [num*i for i in range(1,11)]
+        return table(num)
+
 sol = Solution()
 result = sol.table(5)
 print(result)
 
 table = lambda num: [num * i for i in range(1,11)]
 print(table(5))
+
+result1 = sol.table_lambda(2)
+print(result1)
